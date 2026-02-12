a94057b6a4300599cbc60602b00bc1f8a0e23711
Marc R. <mriverolopez@gmail.com>
2026-02-12 15:54:45 +0100

    pptx: extract presentation part loading

--- crates/docir-parser/src/ooxml/pptx.rs
+++ crates/docir-parser/src/ooxml/pptx.rs
@@ -21,6 +21,7 @@ use std::collections::{HashMap, HashSet};
 use std::io::{Read, Seek};
 
 mod comments;
+mod presentation_parts;
 mod shapes;
 mod slide;
 
@@ -134,97 +135,7 @@ impl PptxParser {
             document.content.push(slide_id);
         }
 
-        // Presentation properties
-        if let Some(props) = parse_xml_part_with_span(
-            zip,
-            "ppt/presProps.xml",
-            parse_presentation_properties,
-            |props, path| props.span = Some(SourceSpan::new(path)),
-        )? {
-            let id = props.id;
-            self.store.insert(IRNode::PresentationProperties(props));
-            document.shared_parts.push(id);
-        }
-
-        // View properties
-        if let Some(props) = parse_xml_part_with_span(
-            zip,
-            "ppt/viewProps.xml",
-            parse_view_properties,
-            |props, path| props.span = Some(SourceSpan::new(path)),
-        )? {
-            let id = props.id;
-            self.store.insert(IRNode::ViewProperties(props));
-            document.shared_parts.push(id);
-        }
-
-        // Table styles
-        if let Some(styles) = parse_xml_part_with_span(
-            zip,
-            "ppt/tableStyles.xml",
-            parse_table_styles,
-            |styles, path| styles.span = Some(SourceSpan::new(path)),
-        )? {
-            let id = styles.id;
-            self.store.insert(IRNode::TableStyleSet(styles));
-            document.shared_parts.push(id);
-        }
-
-        // Comment authors
-        if zip.contains("ppt/commentAuthors.xml") {
-            let authors_xml = zip.read_file_string("ppt/commentAuthors.xml")?;
-            let authors = parse_comment_authors(&authors_xml, "ppt/commentAuthors.xml")?;
-            self.set_comment_authors(&authors);
-            for author in authors {
-                let mut author = author;
-                author.span = Some(SourceSpan::new("ppt/commentAuthors.xml"));
-                let id = author.id;
-                self.store.insert(IRNode::PptxCommentAuthor(author));
-                document.shared_parts.push(id);
-            }
-        }
-
-        // Tags
-        let tag_paths: Vec<String> = zip
-            .file_names()
-            .filter(|p| p.starts_with("ppt/tags/") && p.ends_with(".xml"))
-            .map(|s| s.to_string())
-            .collect();
-        for tag_path in tag_paths {
-            let tag_xml = zip.read_file_string(&tag_path)?;
-            let tags = parse_presentation_tags(&tag_xml, &tag_path)?;
-            for tag in tags {
-                let id = tag.id;
-                self.store.insert(IRNode::PresentationTag(tag));
-                document.shared_parts.push(id);
-            }
-        }
-
-        // People part (coauthoring)
-        if let Some(people) = parse_xml_part_with_span(
-            zip,
-            "ppt/people.xml",
-            crate::ooxml::shared::parse_people_part,
-            |people, path| people.span = Some(SourceSpan::new(path)),
-        )? {
-            let id = people.id;
-            self.store.insert(IRNode::PeoplePart(people));
-            document.shared_parts.push(id);
-        }
-
-        // SmartArt parts
-        let diagram_paths: Vec<String> = zip
-            .file_names()
-            .filter(|p| p.starts_with("ppt/diagrams/") && p.ends_with(".xml"))
-            .map(|s| s.to_string())
-            .collect();
-        for path in diagram_paths {
-            let xml = zip.read_file_string(&path)?;
-            let part = parse_smartart_part(&xml, &path)?;
-            let id = part.id;
-            self.store.insert(IRNode::SmartArtPart(part));
-            document.shared_parts.push(id);
-        }
+        self.load_presentation_parts(&mut document, zip)?;
 
         // Parse slide masters and layouts
         for rel in presentation_rels.get_by_type(rel_type::SLIDE_MASTER) {
