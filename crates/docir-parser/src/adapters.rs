c5613abd875327ccda44095a3bfc3521e84e4391
Marc R. <mriverolopez@gmail.com>
2026-02-13 08:54:45 +0100

    refactor(parser): deduplicate adapter boilerplate

--- crates/docir-parser/src/adapters.rs
+++ crates/docir-parser/src/adapters.rs
@@ -2,97 +2,31 @@ use crate::format::FormatParser;
 use crate::{HwpParser, HwpxParser, OdfParser, OoxmlParser, ParseError, ParserConfig, RtfParser};
 use std::io::{Read, Seek};
 
-pub struct OoxmlAdapter {
-    config: ParserConfig,
+macro_rules! define_adapter {
+    ($adapter:ident, $parser:ty) => {
+        pub struct $adapter {
+            config: ParserConfig,
+        }
+
+        impl $adapter {
+            pub fn new(config: ParserConfig) -> Self {
+                Self { config }
+            }
+        }
+
+        impl FormatParser for $adapter {
+            fn parse_reader<R: Read + Seek>(
+                &self,
+                reader: R,
+            ) -> Result<crate::parser::ParsedDocument, ParseError> {
+                <$parser>::with_config(self.config.clone()).parse_reader(reader)
+            }
+        }
+    };
 }
 
-impl OoxmlAdapter {
-    pub fn new(config: ParserConfig) -> Self {
-        Self { config }
-    }
-}
-
-impl FormatParser for OoxmlAdapter {
-    fn parse_reader<R: Read + Seek>(
-        &self,
-        reader: R,
-    ) -> Result<crate::parser::ParsedDocument, ParseError> {
-        OoxmlParser::with_config(self.config.clone()).parse_reader(reader)
-    }
-}
-
-pub struct OdfAdapter {
-    config: ParserConfig,
-}
-
-impl OdfAdapter {
-    pub fn new(config: ParserConfig) -> Self {
-        Self { config }
-    }
-}
-
-impl FormatParser for OdfAdapter {
-    fn parse_reader<R: Read + Seek>(
-        &self,
-        reader: R,
-    ) -> Result<crate::parser::ParsedDocument, ParseError> {
-        OdfParser::with_config(self.config.clone()).parse_reader(reader)
-    }
-}
-
-pub struct HwpxAdapter {
-    config: ParserConfig,
-}
-
-impl HwpxAdapter {
-    pub fn new(config: ParserConfig) -> Self {
-        Self { config }
-    }
-}
-
-impl FormatParser for HwpxAdapter {
-    fn parse_reader<R: Read + Seek>(
-        &self,
-        reader: R,
-    ) -> Result<crate::parser::ParsedDocument, ParseError> {
-        HwpxParser::with_config(self.config.clone()).parse_reader(reader)
-    }
-}
-
-pub struct HwpAdapter {
-    config: ParserConfig,
-}
-
-impl HwpAdapter {
-    pub fn new(config: ParserConfig) -> Self {
-        Self { config }
-    }
-}
-
-impl FormatParser for HwpAdapter {
-    fn parse_reader<R: Read + Seek>(
-        &self,
-        reader: R,
-    ) -> Result<crate::parser::ParsedDocument, ParseError> {
-        HwpParser::with_config(self.config.clone()).parse_reader(reader)
-    }
-}
-
-pub struct RtfAdapter {
-    config: ParserConfig,
-}
-
-impl RtfAdapter {
-    pub fn new(config: ParserConfig) -> Self {
-        Self { config }
-    }
-}
-
-impl FormatParser for RtfAdapter {
-    fn parse_reader<R: Read + Seek>(
-        &self,
-        reader: R,
-    ) -> Result<crate::parser::ParsedDocument, ParseError> {
-        RtfParser::with_config(self.config.clone()).parse_reader(reader)
-    }
-}
+define_adapter!(OoxmlAdapter, OoxmlParser);
+define_adapter!(OdfAdapter, OdfParser);
+define_adapter!(HwpxAdapter, HwpxParser);
+define_adapter!(HwpAdapter, HwpParser);
+define_adapter!(RtfAdapter, RtfParser);
